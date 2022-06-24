550f76b3395f566e89d5053e00fc9db65b2ef55b
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-06-24 15:55:28 +0900

    Initial commit

---
+++ src/test/java/com/jeognykun/triple/mileage/mileageservice/MileageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeognykun.triple.mileage.mileageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MileageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
