1a85506056c99dfd7c5637ef43e051dd67c39cb6
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-06-27 17:24:03 +0900

    [FEAT] - Delete, Modify Service 클래스 추가

--- src/main/java/com/jeognykun/triple/mileage/mileageservice/handler/EventActionHandler.java
+++ src/main/java/com/jeognykun/triple/mileage/mileageservice/handler/EventActionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class EventActionHandler {
-
     private final EventAddService eventAddService;
 
     public EventActionService actionTypeHandler(EventRequest req){
