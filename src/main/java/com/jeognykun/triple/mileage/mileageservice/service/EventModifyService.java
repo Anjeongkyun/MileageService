edf989135454feeae6eb17b459d8fc15446d7262
Anjeongkyun <dkswjdrbs12@naver.com>
2022-06-28 22:27:37 +0900

    [FEAT]  - EventModifyService 기능 추가

--- src/main/java/com/jeognykun/triple/mileage/mileageservice/service/EventModifyService.java
+++ src/main/java/com/jeognykun/triple/mileage/mileageservice/service/EventModifyService.java
@@ -1,21 +1,37 @@
-//package com.jeognykun.triple.mileage.mileageservice.service;
-//
-//import com.jeognykun.triple.mileage.mileageservice.domain.Event;
-//import com.jeognykun.triple.mileage.mileageservice.dto.EventRequest;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import javax.transaction.Transactional;
-//
-//@Transactional
-//@RequiredArgsConstructor
-//@Service
-//public class EventModifyService implements IEventActionService {
-//
-//    @Override
-//    public Event eventAction(EventRequest req) {
-//
-//
-//        return null;
-//    }
-//}
+package com.jeognykun.triple.mileage.mileageservice.service;
+
+import com.jeognykun.triple.mileage.mileageservice.domain.Event;
+import com.jeognykun.triple.mileage.mileageservice.domain.Place;
+import com.jeognykun.triple.mileage.mileageservice.dto.EventRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class EventModifyService implements IEventActionService {
+
+    private final EventService eventService;
+    private final ReviewSaveService reviewSaveService;
+    private final PlaceService placeService;
+    private final CalculatePointService calculatePointService;
+
+    @Override
+    public Event eventAction(EventRequest req) {
+        eventService.checkContentAndPhotoId(req);
+        reviewSaveService.hasReview(req.getReviewId());
+
+        Place place = placeService.getPlace(req.getPlaceId());
+
+        long contentAndPhotoPoint = calculatePointService.contentAndPhotoPointCalculate(req);
+        long placePoint = placeService.getPlaceMileage(req,place);
+        long mileage = contentAndPhotoPoint + placePoint;
+
+        reviewSaveService.save(req);
+        
+        return eventService.writeEvent(req, mileage);
+
+    }
+}
