f58b5e578d4cd727529b8822a8e26c644b712c27
Anjeongkyun <dkswjdrbs12@naver.com>
2022-06-29 00:23:41 +0900

    [#32] feat: Error Custom Response 로직 추가

---
+++ src/main/java/com/jeognykun/triple/mileage/mileageservice/exception/ExceptionApi.java
@@ -0,0 +1,45 @@
+package com.jeognykun.triple.mileage.mileageservice.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@ResponseBody
+@ControllerAdvice
+public class ExceptionApi {
+    @ExceptionHandler(value = ReviewNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    protected ErrorResponse handleReviewNotFoundException(ReviewNotFoundException e) {
+        final ErrorCode reviewNotFound = ErrorCode.REVIEW_NOT_FOUND;
+        log.error(reviewNotFound.getMessage(), e.getReviewId());
+        return buildError(reviewNotFound);
+    }
+
+    @ExceptionHandler(value = PlaceWriteFailException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ErrorResponse handleDoNotWritePlaceException(PlaceWriteFailException e) {
+        final ErrorCode doNotWrite = ErrorCode.REVIEW_NOT_FOUND;
+        log.error(doNotWrite.getMessage(), e.getPlaceId(), e.getUserId());
+        return buildError(doNotWrite);
+    }
+
+    @ExceptionHandler(value = ContentAndPhotoIdNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ErrorResponse handleContentNotFoundException(ContentAndPhotoIdNotFoundException e) {
+        final ErrorCode contentNotFound = ErrorCode.CONTENT_NOT_FOUNT;
+        log.error(contentNotFound.getMessage(),e.getReviewId());
+        return buildError(contentNotFound);
+    }
+
+    private ErrorResponse buildError(ErrorCode errorCode) {
+        return ErrorResponse.builder()
+                .code(errorCode.getCode())
+                .status(errorCode.getStatus())
+                .message(errorCode.getMessage())
+                .build();
+    }
+}
